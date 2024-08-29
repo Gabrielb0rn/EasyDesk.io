a1caba279ae818fee97c18d2ed3dde0c62b3423b
Gabrielb0rn <161833929+Gabrielb0rn@users.noreply.github.com>
2024-08-29 10:57:30 -0300

    .

--- .history/assets/scripts_20240829105048.js
+++ .history/assets/scripts_20240829105048.js
@@ -137,18 +137,3 @@ document.addEventListener('DOMContentLoaded', function() {
         displayProducts();
     }
 });
-
-// script.js
-document.addEventListener("DOMContentLoaded", function() {
-    const navLinks = document.querySelectorAll('.nav-links a');
-
-    navLinks.forEach(link => {
-        link.addEventListener('mouseover', () => {
-            link.style.color = '#2575fc';
-        });
-
-        link.addEventListener('mouseout', () => {
-            link.style.color = 'white';
-        });
-    });
-});
