9dd9b62022f9b2da58b099420ac499ce197905b2
Gabrielb0rn <161833929+Gabrielb0rn@users.noreply.github.com>
2024-09-05 12:31:52 -0300

    .

---
+++ .history/user-auth-app/bancodedados_20240905122921.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE user_auth_db;
+USE user_auth_db;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
+/
