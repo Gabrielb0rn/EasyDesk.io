cff2534e2c4f845322d99d72fe337fa708271897
Gabrielb0rn <161833929+Gabrielb0rn@users.noreply.github.com>
2024-09-12 13:15:00 -0300

    .

--- assets/node-user/login.js
+++ assets/node-user/login.js
@@ -1,27 +1,33 @@
 function irParaCadastro() {
-    document.location.href = './cadastro.html'
-  }
+    document.location.href = './cadastro.html';
+}
 
-  function login() {
-      const name = document.getElementById('name').value;
-      const password = document.getElementById('password').value;
+function login() {
+    const name = document.getElementById('name').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
 
-// EXERCICIO verificar se o usuário digitou o nome e a senha antes de buscar o login do localStorage
+    // EXERCICIO verificar se o usuário digitou o nome e a senha antes de buscar o login do localStorage
+    if (!name || !password) {
+        errorMessage.style.display = 'block';
+        errorMessage.textContent = 'Preencha todos os campos corretamente.';
+        return;
+    } else {
+        errorMessage.style.display = 'none';
+    }
 
-      const obj = {
-          name: name,
-          password: password
-      };
+    const obj = {
+        name: name,
+        password: password
+    };
 
-      const login = JSON.parse(localStorage.getItem('login'));
+    const login = JSON.parse(localStorage.getItem('login'));
 
-      if (obj.name === login.name && obj.password === login.password) {
-
-// EXERCICIO redirecionar o usuário para a tela de bem vindo 
-
-      document.location.href = 'https://gabrielb0rn.github.io/EasyDesk.io/';
-      } else {
-        alert('Usuário ou senha inválidos');
-      }
-
-  }
+    // EXERCICIO redirecionar o usuário para a tela de bem vindo -- 
+    if (login && obj.name === login.name && obj.password === login.password) {
+        document.location.href = 'https://gabrielb0rn.github.io/EasyDesk.io/';
+    } else {
+        errorMessage.style.display = 'block';
+        errorMessage.textContent = 'Usuário ou senha inválidos.';
+    }
+}
