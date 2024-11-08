d52ff6653e55ed1775add20f97fac3c48e716820
Gabrielb0rn <161833929+Gabrielb0rn@users.noreply.github.com>
2024-11-08 09:53:13 -0300

    .

--- 0inativo/assets-adm/script.js
+++
@@ -1,6 +0,0 @@
-// scripts.js
-
-document.querySelector('.login-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Login realizado com sucesso!');
-});
--- 0inativo/node-user/extra.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    const loginData = JSON.parse(localStorage.getItem('login'));
-    
-    if (loginData && loginData.name) {
-        document.getElementById('user-info').innerHTML = `<b>Usuário:</b> ${loginData.name}`;
-    } else {
-        document.getElementById('user-info').innerHTML = `<b>Nenhum usuário logado.</b>`;
-    }
-}
