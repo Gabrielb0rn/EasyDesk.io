154393d95e322f963872684a4dcd1b1e83bfa786
Gabrielb0rn <161833929+Gabrielb0rn@users.noreply.github.com>
2024-11-08 09:56:28 -0300

    .

---
+++ assets/,js/modules/form/form.js
@@ -0,0 +1,10 @@
+// form.js
+
+// Lógica específica para formulários que não se aplica globalmente
+function handleFormSubmit(event) {
+    event.preventDefault();
+    // Lógica para lidar com o envio do formulário
+}
+
+// Exporte as funções se estiver usando módulos ES6
+// export { handleFormSubmit };
